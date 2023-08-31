fc204434a4aecb8b6b069129f7ed87dea1a60f4a
Genosse_Stalin <artemlobas86@gmail.com>
2023-08-31 17:53:03 +0300

    Initial Commit

--- main.py
+++ main.py
@@ -3,69 +3,34 @@ import requests
 from tkinter import *
 
 city = 'Сочи'
-app_id = "79d1ca96933b0328e1c7e3e7a26cb347"
+app_id = "edb13ef869842c8a83c691c532ecedf0"
 information_oneDay = []
-information_fiveDay = []
+tempMin_fiveDay = []
+tempMax_fiveDay = []
+weather_fiveDay = []
+weather_icon = []
 
 
 def WeatherReportOneDay():
-    global res
-    global data
-    global city
-    global information_oneDay
-    global y
-    global x
-    city_get = EnterCity.get()
-    if city_get != '':
-        city = city_get.title()
-
-        EnterCity.delete(0, END)
-
-        Label(window, text=city, font=('Times New Roman', 17, 'bold'), background='#40E0D0').place(x=1, y=50)
-
-        url_ = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=ru&appid' \
-                                                                             '=91f2ab829c963c4a7414b33cd4ee44ad'
-
-        weather_data_ = requests.get(url_).json()
-
-        temperature['text'] = 'Температура сейчас: ' + str(round(weather_data_['main']['temp'])) + '°C'
-        temperature.place()
-        information_oneDay.append('Температура сейчас: ' + str(round(weather_data_['main']['temp'])) + '°C')
-        temperature_feels['text'] = 'Ощущается как: ' + str(round(weather_data_['main']['feels_like'])) + '°C'
-        temperature_feels.place()
-        information_oneDay.append('Ощущается как: ' + str(round(weather_data_['main']['feels_like'])) + '°C')
-
-        pressure['text'] = 'Давление воздуха: ' + str(round(weather_data_['main']['pressure'] * 0.75)) + ' мм рт с'
-        pressure.place()
-
-        information_oneDay.append('Давление воздуха: ' + str(round(weather_data_['main']['pressure'] * 0.75)) + 'мм рт '
-                                                                                                                'с')
-        humidity['text'] = 'Влажность воздуха(%): ' + str(round(weather_data_['main']['humidity'])) + '%'
-        humidity.place()
-
-        information_oneDay.append('Влажность воздуха(%): ' + str(round(weather_data_['main']['humidity'])) + '%')
-        wind_speed['text'] = 'Скорость ветра: ' + str(round(weather_data_['wind']['speed'])) + ' м/с'
-
-        res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
-                           params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': app_id})
-        data = res.json()
-        y = 100
-        x = 400
-
-        for i in data['list']:
-            if y >= 500:
-                x = 700
-                y = 100
-            information_fiveDay.append(str(i['dt_txt']) + ' ' + str('{0:+3.0f}'.format(i['main']['temp'])) + ' ' +
-                                       str(i['weather'][0]['description']))
-            Label(text=str(i['dt_txt']) + ' ' + str('{0:+3.0f}'.format(i['main']['temp'])) + ' ' + str(
-                i['weather'][0]['description']), font=('Times New Roman', 11), background='#40E0D0').place(x=x, y=y)
-            y += 20
+    pass
+#     global res
+#     global data
+#     global city
+#     global information_oneDay
+#     global y
+#     global x
+#     city_get = EnterCity.get()
+#     if city_get != '':
+#         city = city_get.title()
+#
+#         EnterCity.delete(0, END)
+#         sityLable['text'] = city
+#         sityLable.place(x=1, y=50)
 
 
 window = Tk()
 
-window.geometry('990x600')
+window.geometry('600x950')
 window.config(background='#40E0D0')
 
 EnterCity = Entry(font=('Times New Roman', 17), width=35, background='#5F9EA0')
@@ -73,59 +38,60 @@ EnterCity.place(x=1, y=1)
 
 Button(text='Посмотреть прогноз', font=('Times New Roman', 12), command=WeatherReportOneDay, width=20,
        background='#20B2AA', activebackground='#008B8B').place(x=370, y=0)
-Label(text='Прогноз на пять дней', font=('Times New Roman', 17, 'bold'), background='#40E0D0').place(x=400, y=50)
-
-temperature = Label(text='Температура сейчас:', font=('Times New Roman', 15), background='#40E0D0')
-temperature.place(x=5, y=100)
-
-temperature_feels = Label(text='Ощущается как:', font=('Times New Roman', 15), background='#40E0D0')
-temperature_feels.place(x=5, y=150)
-
-pressure = Label(text='Давление воздуха:', font=('Times New Roman', 15), background='#40E0D0')
-pressure.place(x=5, y=200)
-
-humidity = Label(text='Влажность воздуха(%):', font=('Times New Roman', 15), background='#40E0D0')
-humidity.place(x=5, y=250)
-
-wind_speed = Label(text='Скорость ветра:', font=('Times New Roman', 15), background='#40E0D0')
-wind_speed.place(x=5, y=300)
 
 url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=ru&appid' \
-                                                                    '=91f2ab829c963c4a7414b33cd4ee44ad'
+                                                                    '=edb13ef869842c8a83c691c532ecedf0'
+
+url2 = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=metric&lang=ru&appid' \
+                                                                      '=edb13ef869842c8a83c691c532ecedf0'
 
 weather_data = requests.get(url).json()
-Label(window, text=city, font=('Times New Roman', 17, 'bold'), background='#40E0D0').place(x=1, y=50)
 
-temperature['text'] = 'Температура сейчас: ' + str(round(weather_data['main']['temp'])) + '°C'
-temperature.place()
-information_oneDay.append('Температура сейчас: ' + str(round(weather_data['main']['temp'])) + '°C')
-temperature_feels['text'] = 'Ощущается как: ' + str(round(weather_data['main']['feels_like'])) + '°C'
-temperature_feels.place()
-information_oneDay.append('Ощущается как: ' + str(round(weather_data['main']['feels_like'])) + '°C')
-pressure['text'] = 'Давление воздуха: ' + str(round(weather_data['main']['pressure'] * 0.75)) + ' мм рт с'
-pressure.place()
-information_oneDay.append('Давление воздуха: ' + str(round(weather_data['main']['pressure'] * 0.75)) + 'мм рт '
-                                                                                                       'с')
-humidity['text'] = 'Влажность воздуха(%): ' + str(round(weather_data['main']['humidity'])) + '%'
-humidity.place()
-information_oneDay.append('Влажность воздуха(%): ' + str(round(weather_data['main']['humidity'])) + '%')
-wind_speed['text'] = 'Скорость ветра: ' + str(round(weather_data['wind']['speed'])) + ' м/с'
+print(weather_data)
+cityLabel = Label(window, text=city, font=('Times New Roman', 30, 'bold'), background='#40E0D0')
+cityLabel.place(x=1, y=50)
+weather_5day = requests.get(url2).json()
+print(weather_5day)
+j = 5
 
-res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
-                   params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': app_id})
-data = res.json()
+
+x = 10
 y = 100
-x = 400
-for i in data['list']:
-    if y >= 500:
-        x = 700
-        y = 100
-    information_fiveDay.append(str(i['dt_txt']) + ' ' + str('{0:+3.0f}'.format(i['main']['temp'])) + ' ' +
-                               str(i['weather'][0]['description']))
-    Label(text=str(i['dt_txt']) + ' ' + str('{0:+3.0f}'.format(i['main']['temp'])) + ' ' + str(
-        i['weather'][0]['description']), font=('Times New Roman', 11), background='#40E0D0').place(x=x, y=y)
-    y += 20
-weatherDataStructure = json.dumps(weather_data, indent=2)
-print(information_oneDay)
+
+for i in range(30):
+    l = Label(window, font=('Times New Roman', 17, 'bold'), background='#40E0D0')
+    l.place(x=x, y=y)
+    information_oneDay.append(l)
+    y += 50
+
+information_oneDay[0]['text'] = f"Погодное явление: {weather_data['weather'][0]['description']}"
+information_oneDay[1]['text'] = f"Температура: {round(weather_data['main']['temp'])}°C"
+information_oneDay[2]['text'] = f"Ощущается как: {round(weather_data['main']['feels_like'])}°C"
+information_oneDay[3]['text'] = f"Видимость: {round(weather_data['visibility'])}м"
+information_oneDay[4]['text'] = f"Скорость ветра: {round(weather_data['wind']['speed'])}м/с"
+
+
+for i in weather_5day['list']:
+    tempMin_fiveDay.append(round(i['main']['temp_min']))
+    tempMax_fiveDay.append(round(i['main']['temp_max']))
+    weather_fiveDay.append(i['weather'][0]['description'])
+    weather_icon.append(i['weather'][0]['icon'])
+
+for i in range(5, 13):
+    information_oneDay[i]['text'] = tempMin_fiveDay[i]
+    information_oneDay[i + 8]['text'] = tempMax_fiveDay[i]
+    information_oneDay[i]['font'] = ('Times New Roman', 13, 'bold')
+    information_oneDay[i + 8]['font'] = ('Times New Roman', 13, 'bold')
+    information_oneDay[i + 16]['font'] = ('Times New Roman', 13, 'bold')
+    information_oneDay[i].place(y=370, x=x)
+    information_oneDay[i + 8].place(y=390, x=x)
+    information_oneDay[i + 16].place(y=350, x=x)
+    x += 50
+
+for i in range(5, 13):
+    image = PhotoImage(f"{weather_icon[i]}@2x.png")
+    information_oneDay[i + 16] = Label(image=image)
+    information_oneDay[i + 16].place(y=350, x=x)
+
 
 window.mainloop()
