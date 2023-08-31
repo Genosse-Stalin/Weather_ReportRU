8bff446dac8965fa22056b328f663ce949488556
Genosse_Stalin <artemlobas86@gmail.com>
2023-08-31 19:59:02 +0300

    Initial Commit

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ weather_icon = []
 
 def WeatherReportOneDay():
     pass
+
+
 #     global res
 #     global data
 #     global city
@@ -39,13 +41,14 @@ EnterCity.place(x=1, y=1)
 Button(text='Посмотреть прогноз', font=('Times New Roman', 12), command=WeatherReportOneDay, width=20,
        background='#20B2AA', activebackground='#008B8B').place(x=370, y=0)
 
-url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=ru&appid' \
-                                                                    '=edb13ef869842c8a83c691c532ecedf0'
+url = 'https://api.openweathermap.org/data/2.5/weather'
+      # 'q=' + city + '&units=metric&lang=ru&appid' \
+      #                                                               '=edb13ef869842c8a83c691c532ecedf0'
 
 url2 = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=metric&lang=ru&appid' \
                                                                       '=edb13ef869842c8a83c691c532ecedf0'
 
-weather_data = requests.get(url).json()
+weather_data = requests.get(url, 'q=' + city + '&units=metric&lang=ru&appid=edb13ef869842c8a83c691c532ecedf0').json()
 
 print(weather_data)
 cityLabel = Label(window, text=city, font=('Times New Roman', 30, 'bold'), background='#40E0D0')
@@ -53,7 +56,6 @@ cityLabel.place(x=1, y=50)
 weather_5day = requests.get(url2).json()
 print(weather_5day)
 j = 5
-
 
 x = 10
 y = 100
@@ -69,7 +71,6 @@ information_oneDay[1]['text'] = f"Температура: {round(weather_data['m
 information_oneDay[2]['text'] = f"Ощущается как: {round(weather_data['main']['feels_like'])}°C"
 information_oneDay[3]['text'] = f"Видимость: {round(weather_data['visibility'])}м"
 information_oneDay[4]['text'] = f"Скорость ветра: {round(weather_data['wind']['speed'])}м/с"
-
 
 for i in weather_5day['list']:
     tempMin_fiveDay.append(round(i['main']['temp_min']))
@@ -87,11 +88,5 @@ for i in range(5, 13):
     information_oneDay[i + 8].place(y=390, x=x)
     information_oneDay[i + 16].place(y=350, x=x)
     x += 50
-
-for i in range(5, 13):
-    image = PhotoImage(f"{weather_icon[i]}@2x.png")
-    information_oneDay[i + 16] = Label(image=image)
-    information_oneDay[i + 16].place(y=350, x=x)
-
 
 window.mainloop()
